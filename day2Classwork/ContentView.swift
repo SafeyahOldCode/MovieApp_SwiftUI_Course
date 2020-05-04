0e505a639306b9da81bb798b8a798608e8f08a23
SafeyahShemali <safonorthly@gmail.com>
2020-05-04 22:46:38 +0300

    Initial Commit

---
+++ day2Classwork/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  day2Classwork
+//
+//  Created by Safeyah on 5/4/20.
+//  Copyright © 2020 Safeyah Coding. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
